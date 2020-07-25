7e863548cc4934fd52596f5e8ed3ebba219de4b7
Ben Ryan <benryan03@gmail.com>
2020-07-24 23:48:39 -0400

    Added rotaton for line piece

--- Form1.cs
+++ Form1.cs
@@ -16,6 +16,8 @@ namespace Tetris
         Control[] activePiece = { null, null, null, null };
         Control[] activePiece2 = { null, null, null, null };
         int timeElapsed = 0;
+        int currentPiece;
+        int rotations = 0;
 
         public Form1()
         {
@@ -26,24 +28,27 @@ namespace Tetris
 
         public void dropNewPiece()
         {
+            rotations = 0;
             System.Random random = new System.Random();
-            int rand = random.Next(7);
+            //currentPiece = random.Next(7);
 
-            if (rand == 0)
+            currentPiece = 0;
+
+            if (currentPiece == 0)
             {
                 activePiece[0] = pictureBox6;
                 activePiece[1] = pictureBox16;
                 activePiece[2] = pictureBox26;
                 activePiece[3] = pictureBox36;
             }
-            else if (rand == 1)
+            else if (currentPiece == 1)
             {
                 activePiece[0] = pictureBox4;
                 activePiece[1] = pictureBox14;
                 activePiece[2] = pictureBox24;
                 activePiece[3] = pictureBox25;
             }
-            else if (rand == 2)
+            else if (currentPiece == 2)
             {
                 activePiece[0] = pictureBox5;
                 activePiece[1] = pictureBox15;
@@ -51,28 +56,28 @@ namespace Tetris
                 activePiece[3] = pictureBox24;
 
             }
-            else if (rand == 3)
+            else if (currentPiece == 3)
             {
                 activePiece[0] = pictureBox5;
                 activePiece[1] = pictureBox6;
                 activePiece[2] = pictureBox14;
                 activePiece[3] = pictureBox15;
             }
-            else if (rand == 4)
+            else if (currentPiece == 4)
             {
                 activePiece[0] = pictureBox5;
                 activePiece[1] = pictureBox6;
                 activePiece[2] = pictureBox16;
                 activePiece[3] = pictureBox17;
             }
-            else if (rand == 5)
+            else if (currentPiece == 5)
             {
                 activePiece[0] = pictureBox5;
                 activePiece[1] = pictureBox6;
                 activePiece[2] = pictureBox15;
                 activePiece[3] = pictureBox16;
             }
-            else if (rand == 6)
+            else if (currentPiece == 6)
             {
                 activePiece[0] = pictureBox6;
                 activePiece[1] = pictureBox15;
@@ -209,7 +214,6 @@ namespace Tetris
             {
                 movePiece("left");
             }
-
             else if (e.KeyCode == Keys.Right & testMove("right") == true)
             {
                 movePiece("right");
@@ -221,6 +225,104 @@ namespace Tetris
             else if (e.KeyCode == Keys.Up)
             {
                 //Rotate
+
+                int square1Col = grid.GetColumn(activePiece[0]);
+                int square1Row = grid.GetRow(activePiece[0]);
+
+                int square2Col = grid.GetColumn(activePiece[1]);
+                int square2Row = grid.GetRow(activePiece[1]);
+
+                int square3Col = grid.GetColumn(activePiece[2]);
+                int square3Row = grid.GetRow(activePiece[2]);
+
+                int square4Col = grid.GetColumn(activePiece[3]);
+                int square4Row = grid.GetRow(activePiece[3]);
+
+                bool canMove = true;
+
+                if (currentPiece == 0)
+                {
+                    //The line piece
+
+                    if (rotations == 0)
+                    {
+                        if (square1Col == 0 | square1Col == 1 | square1Col == 9)
+                        { 
+                            canMove = false;
+                        }
+                    }
+                    else if (rotations == 1)
+                    {
+                        if (square3Col == 0 | square3Col == 1 | square3Col == 9)
+                        {
+                            canMove = false;
+                        }
+                    }
+
+                    if (canMove == true)
+                    {
+                        if (rotations == 0)
+                        {
+                            activePiece2[0] = grid.GetControlFromPosition(square1Col - 2, square1Row);
+                            activePiece2[1] = grid.GetControlFromPosition(square2Col - 1, square2Row - 1);
+                            activePiece2[2] = grid.GetControlFromPosition(square3Col, square3Row - 2);
+                            activePiece2[3] = grid.GetControlFromPosition(square4Col + 1, square4Row - 3);
+                            rotations++;
+                        }
+                        else if (rotations == 1)
+                        {
+                            activePiece2[0] = grid.GetControlFromPosition(square1Col + 2, square1Row);
+                            activePiece2[1] = grid.GetControlFromPosition(square2Col + 1, square2Row + 1);
+                            activePiece2[2] = grid.GetControlFromPosition(square3Col, square3Row + 2);
+                            activePiece2[3] = grid.GetControlFromPosition(square4Col - 1, square4Row + 3);
+                            rotations = 0;
+                        }
+
+                        
+                    }
+                }
+                else if (currentPiece == 1)
+                {
+                    //The normal L
+
+                }
+                else if (currentPiece == 2)
+                {
+                    //The backwards L
+
+                }
+                else if (currentPiece == 3)
+                {
+                    //The normal S
+
+                }
+                else if (currentPiece == 4)
+                {
+                    //The backwards S
+
+                }
+                else if (currentPiece == 5)
+                {
+                    //The square
+
+                }
+                else if (currentPiece == 6)
+                {
+                    //The pyramid
+                }
+
+
+                foreach (PictureBox square in activePiece)
+                {
+                    square.BackColor = Color.White;
+                }
+                int x = 0;
+                foreach (PictureBox square in activePiece2)
+                {
+                    square.BackColor = Color.Red;
+                    activePiece[x] = square;
+                    x++;
+                }
             }
 
         }
