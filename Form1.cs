e3d431265dafa9b54bfea89dc25a943a2418db10
Ben Ryan <benryan03@gmail.com>
2020-10-05 20:44:38 -0400

    Removed unneeded namespace declarations

--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics.Eventing.Reader;
-using System.Diagnostics.Tracing;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
 
 namespace Tetris
 {
